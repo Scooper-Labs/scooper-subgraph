9f4d9a97418feef7b632f4f83b985a843fc698d6
MayowaObisesan <mayowaobi74@gmail.com>
2024-10-09 06:31:13 -0700

    Update the graph connection for assetscooper

--- src/asset-scooper.ts
+++ src/asset-scooper.ts
@@ -5,8 +5,9 @@ export function handleTokenSwapped(event: TokenSwappedEvent): void {
   let entity = new TokenSwapped(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
+
   entity.user = event.params.user
-  entity.tokenIn = event.params.tokenIn
+  entity.tokenIn = event.params.tokenA
   entity.amountIn = event.params.amountIn
   entity.amountOut = event.params.amountOut
 
